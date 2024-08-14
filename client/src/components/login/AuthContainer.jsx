4c3fcce8b6c9ad96d4cb97526b182de27208cfcb
Julian Jimenez <julianjimenez2128@gmail.com>
2024-08-14 10:25:00 -0500

    upload project

--- client/src/components/login/AuthContainer.jsx
+++ client/src/components/login/AuthContainer.jsx
@@ -1,10 +1,8 @@
-import React from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
-import LoginForm from "./LoginForm";
-import RegisterForm from "./RegisterForm";
-
+// src/components/login/AuthContainer.jsx
+import React, {useContext} from "react";
+import { RenderContext } from "../../contexts/renderContext";
 function AuthContainer() {
-  const navigate = useNavigate();
+  const {  renderComponent } = useContext(RenderContext);
 
   return (
     <div className="p-0">
@@ -27,18 +25,19 @@ function AuthContainer() {
             Al registrarte, podrás guardar tus notas y verlas cuando quieras. Si
             ya estás registrado, inicia sesión.
           </p>
+          <div className="col-1"></div>
           <div className="row">
             <div className="col-12 col-md-6">
               <button
-                onClick={() => navigate("/login/login")}
+                onClick={() => renderComponent("LoginForm")}
                 className="w-100 p-2 btn btn-primary mb-2 fw-bolder"
               >
-                Iniciar Sesión
+                Iniciar Sesion
               </button>
             </div>
             <div className="col-12 col-md-6">
               <button
-                onClick={() => navigate("/login/register")}
+                onClick={() => renderComponent("RegisterForm")}
                 className="w-100 p-2 btn btn-light mb-2 border-secondary text-primary fw-bolder"
               >
                 Registrarme
@@ -46,16 +45,23 @@ function AuthContainer() {
             </div>
           </div>
         </div>
-        <div className="col-1"></div>
+        <div className="col-4"></div>
       </div>
       <div className="row">
         <div className="col-1"></div>
         <div className="col-10 bg-white text-info align-content-center border border-1 border-primary auth-cols rounded-bottom-3">
-          <Routes>
-            <Route path="login" element={<LoginForm />} />
-            <Route path="register" element={<RegisterForm />} />
-            <Route path="/" element={<p>Selecciona una opción para continuar.</p>} />
-          </Routes>
+          <p className="text-dark mt-3">
+            Al registrarte, podrás guardar tus notas y verlas cuando quieras. Si
+            ya estás registrado, inicia sesión.
+          </p>
+          <div className="col-1"></div>
+          <div className="row">
+            <div className="col-12">
+              <button className="w-100 p-2 btn btn-outline-danger mb-2 border-secondary fw-bolder mt-3 mb-4">
+                Iniciar sin Registrarme
+              </button>
+            </div>
+          </div>
         </div>
         <div className="col-4"></div>
       </div>

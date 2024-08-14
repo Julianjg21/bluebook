ca8bc99d761d3b6c11e2a2e3bcd5bee0ea66b78d
Julian Jimenez <julianjimenez2128@gmail.com>
2024-08-14 08:40:52 -0500

    jkfekjskjewfds

--- Server.mjs
+++ Server.mjs
@@ -1,9 +1,14 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import dotenv from 'dotenv';
+import cors from "cors";
+
+const PORT =  process.env.PORT ||  3080;
 
 const app = express();
-
+dotenv.config();
+app.use(cors()); 
 //Get the name of the current file and directory
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,7 +19,7 @@ const publicPath = path.join(__dirname, 'server/client/build');
 app.use(express.static(publicPath));
 
 //Start the server
-app.listen(2128, (err) => {
+app.listen(PORT, (err) => {
   if (err) {
     console.error(`Error al iniciar el servidor: ${err}`);
   } else {

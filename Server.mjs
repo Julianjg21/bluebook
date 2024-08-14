53509c18a1b367b9a61f80d53d5cb1686f04446c
Julian Jimenez <julianjimenez2128@gmail.com>
2024-08-14 08:58:16 -0500

    server working

--- Server.mjs
+++ Server.mjs
@@ -14,12 +14,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 //Path to 'public' directory inside 'client'
-const publicPath = path.join(__dirname, 'server/client/build');
+const publicPath = path.join(__dirname, 'client/build');
 //Configure the middleware to serve static filesy
 app.use(express.static(publicPath));
 
 //Start the server
-app.listen(PORT, (err) => {
+app.listen(PORT, (err) => { 
   if (err) {
     console.error(`Error al iniciar el servidor: ${err}`);
   } else {
